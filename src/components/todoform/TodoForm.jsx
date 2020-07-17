086a35fa2b5a7a3fae2805c4e1b1c3eae2815258
ican21 <39551278+ican21@users.noreply.github.com>
2020-07-17 23:24:23 +0800

    menambahkan module css ke todoform

---
+++ src/components/todoform/TodoForm.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./todoForm.module.css";
+const TodoForm = ({ addTodo, showAdd }) => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    if (!value) {
+      alert("No Blank Todo!");
+      return;
+    }
+    if (value.length > 40) {
+      alert("Please create a shorter todo text");
+      setValue("");
+      return;
+    }
+    addTodo(value);
+    setValue("");
+  };
+  if (showAdd) {
+    return (
+      <section className={styles.add}>
+        <form className={styles.addForm} onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            className={styles.addInput}
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
+          <button className={styles.addBtn}>Add</button>
+        </form>
+      </section>
+    );
+  } else {
+    return null;
+  }
+};
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  showAdd: PropTypes.bool.isRequired
+};
+export default TodoForm;
