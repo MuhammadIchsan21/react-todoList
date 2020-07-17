4531bbb3926dc00e86895f3ed30f68eeb933a2b5
ican21 <39551278+ican21@users.noreply.github.com>
2020-07-17 22:23:04 +0800

    menambahkan header modules css

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "../button/Button";
+import styles from "./header.module.css";
+const Header = ({ showAddToggle, showAdd, clearTodos }) => {
+  return (
+    <section className={styles.header}>
+      <Button
+        text={showAdd ? "Finish" : "Add"}
+        onClick={showAddToggle}
+        align="left"
+      />
+      <h1 className={styles.headerTitle}>Todo Lists</h1>
+      <Button text="Clear" onClick={clearTodos} color="red" align="right" />
+    </section>
+  );
+};
+Header.propTypes = {
+  showAddToggle: PropTypes.func.isRequired,
+  showAdd: PropTypes.bool.isRequired,
+  clearTodos: PropTypes.func.isRequired
+};
+export default Header;
