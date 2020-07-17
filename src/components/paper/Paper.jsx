86e509a45c579058ddb7467c2d5c6fc137e7e509
ican21 <39551278+ican21@users.noreply.github.com>
2020-07-17 23:06:40 +0800

    menambahkan paper module css

---
+++ src/components/paper/Paper.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./paper.module.css";
+const Paper = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.frame}>{children}</div>
+    </div>
+  );
+};
+Paper.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
+};
+export default Paper;
